586e6bba6a23aa20fd47a7177de132b851987aa1
syler11 <nemeth.szilard82@gmail.com>
2021-12-02 14:22:53 +0000

    added hide and reveal function to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+/* Hide certain dom elements on page load */
+
+
+    document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('levelSelector').classList.add('no-display');
     document.getElementById('gameBoard').classList.add('no-display');
     document.getElementById('gameOutput').classList.add('no-display');
 });
+
+/* Hide and reveal certain dom elemtns once the game level is selected */
+
+function displayGame() {
+    document.getElementById('levelSelector').classList.remove('no-display');
+    document.getElementById('gameBoard').classList.remove('no-display');
+    document.getElementById('gameOutput').classList.remove('no-display');
+    document.getElementById('howto-section').remove();
+    document.getElementById('loadGame').remove();
+}
+
+document.getElementById('easy-game').addEventListener('click', loadEasyGame);
+
+function loadEasyGame() {
+    displayGame();
+}
