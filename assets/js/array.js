c186596ef1fd6c79337a3b092bef2f8ab16225c3
syler11 <nemeth.szilard82@gmail.com>
2021-12-04 12:27:24 +0000

    Card array was moved from main.js to array.js

---
+++ assets/js/array.js
@@ -0,0 +1,23 @@
+/* Animal card array */
+
+const animalCards = [
+    { name: 'dog', img: './assets/images/dog.png', },
+    { name: 'dog', img: './assets/images/dog.png', },
+    { name: 'cat', img: './assets/images/cat.png', },
+    { name: 'cat', img: './assets/images/cat.png', },
+    { name: 'turtle', img: './assets/images/turtle.png', },
+    { name: 'turtle', img: './assets/images/turtle.png', },
+    { name: 'lion', img: './assets/images/lion.png', },
+    { name: 'lion', img: './assets/images/lion.png', },
+    { name: 'elephant', img: './assets/images/elephant.png', },
+    { name: 'elephant', img: './assets/images/elephant.png', },
+    { name: 'giraffe', img: './assets/images/giraffe.png', },
+    { name: 'giraffe', img: './assets/images/giraffe.png', },
+    { name: 'kangaroo', img: './assets/images/kangaroo.png', },
+    { name: 'kangaroo', img: './assets/images/kangaroo.png', },
+    { name: 'frog', img: './assets/images/frog.png', },
+    { name: 'frog', img: './assets/images/frog.png', },
+    { name: 'monkey', img: './assets/images/monkey.png', },
+    { name: 'monkey', img: './assets/images/monkey.png', },
+
+];
