cb247841c86873e4603d713962d0e9fe7d26ed0d
syler11 <nemeth.szilard82@gmail.com>
2021-12-05 19:49:00 +0000

    easygame.js added to index.html

--- assets/js/easygame.js
+++ assets/js/easygame.js
@@ -1,96 +1,96 @@
 
-/* This statement will make sure only 12 cards will be displayed */
-const animalCardMedium = animalCards.slice(0, 12);
+/* This statement will make sure only 6 cards will be displayed */
+const animalCardEasy = animalCards.slice(0, 6);
 
 /* Button wil initiate the launch of the medium level game */
-document.getElementById('medium-game').addEventListener('click', loadMediumGame);
+document.getElementById('easy-game').addEventListener('click', loadEasyGame);
 
 
 /* Loading the game after selecting the level on the index page */
-function loadMediumGame() {
+function loadEasyGame() {
     displayGame();
-    launchGameBoardMedium();
+    launchGameBoardEasy();
     stepsCount.innerHTML = `0`;
     scoreCount.innerHTML = `0`;
     setInterval(setTimer, 800);
 }
 
 /* Variables for medium level game */
-var animalCardMediumSelected = [];
-var animalCardMediumSelectedId = [];
-var animalCardMediumCorrect = [];
+var animalCardEasySelected = [];
+var animalCardEasySelectedId = [];
+var animalCardEasyCorrect = [];
 
 /* Creating gameboard with showing back of the card */
-function launchGameBoardMedium() {
-    animalCardMedium.sort(() => 0.5 - Math.random());
-    for (let i = 0; i < animalCardMedium.length; i++) {
-        var animalCardMed = document.createElement('img');
-        animalCardMed.setAttribute('src', './assets/images/question-mark.png');
-        animalCardMed.setAttribute('data-id', i);
-        animalCardMed.setAttribute('alt', 'Card back, select to turn');
-        animalCardMed.classList.add('col-4', 'col-lg-2', 'animalCard'); /* Use this to resize the gameBoard area */
-        animalCardMed.addEventListener('click', turnAnimalCardMedium);
-        gameGrid.appendChild(animalCardMed);
+function launchGameBoardEasy() {
+    animalCardEasy.sort(() => 0.5 - Math.random());
+    for (let i = 0; i < animalCardEasy.length; i++) {
+        var animalCardEazy = document.createElement('img');
+        animalCardEazy.setAttribute('src', './assets/images/question-mark.png');
+        animalCardEazy.setAttribute('data-id', i);
+        animalCardEazy.setAttribute('alt', 'Card back, select to turn');
+        animalCardEazy.classList.add('col-4', 'col-lg-2', 'animalCard'); /* Use this to resize the gameBoard area */
+        animalCardEazy.addEventListener('click', turnAnimalCardMedium);
+        gameGrid.appendChild(animalCardEazy);
     }
 }
 
 /* Turns cards by clicking  Credit: Ania Kubow */
-function turnAnimalCardMedium() {
-    var animalCardMediumId = this.getAttribute('data-id');
-    animalCardMediumSelected.push(animalCardMedium[animalCardMediumId].name);
-    animalCardMediumSelectedId.push(animalCardMediumId);
-    this.setAttribute('alt', animalCardMedium[animalCardMediumId].name);
-    this.setAttribute('src', animalCardMedium[animalCardMediumId].img);
+function turnAnimalCardEasy() {
+    var animalCardEasyId = this.getAttribute('data-id');
+    animalCardEasySelected.push(animalCardEasy[animalCardEasyId].name);
+    animalCardEasySelectedId.push(animalCardEasyId);
+    this.setAttribute('alt', animalCardEasy[animalCardEasyId].name);
+    this.setAttribute('src', animalCardEasy[animalCardEasyId].img);
 
-    if (animalCardMediumSelected.length === 2) {
+    if (animalCardEasySelected.length === 2) {
         setTimeout(checkMatch, 300);
-    } else if (animalCardMediumSelected.length > 2) {
+    } else if (animalCardEasySelected.length > 2) {
         this.setAttribute('src', './assets/images/question-mark.png');
     }
     /* Limits card turns to two before it turn back unless they are a match */
-    animalCardMediumSelected.length = Math.min(animalCardMediumSelected.length, 2);
+    animalCardEasySelected.length = Math.min(animalCardEasySelected.length, 2);
 }
 
 function checkMatchMedium() {
-    var animalCardMedium = document.querySelectorAll('img');
-    const animalCardMediumFirst = animalCardMediumSelectedId[0];
-    const animalCardMediumSecond = animalCardMediumSelectedId[1];
+    var animalCardEasy = document.querySelectorAll('img');
+    const animalCardEasyFirst = animalCardEasySelectedId[0];
+    const animalCardEasySecond = animalCardEasySelectedId[1];
 
-    if (animalCardMediumSelected[0] === animalCardMediumSelected[1] && animalCardMediumFirst !== animalCardMediumSecond) {
-        animalCardMediumCorrect.push(animalCardMediumSelected);
+    if (animalCardEasySelected[0] === animalCardEasySelected[1] && animalCardEasyFirst !== animalCardEasySecond) {
+        animalCardEasyCorrect.push(animalCardEasySelected);
         stepsCounter();
-        animalCardMedium[animalCardMediumFirst].removeEventListener("click", turnAnimalCard);
-        animalCardMedium[animalCardMediumSecond].removeEventListener("click", turnAnimalCard);
-        animalCardMedium[animalCardMediumFirst].classList.add('match');
-        animalCardMedium[animalCardMediumSecond].classList.add('match');
+        animalCardEasy[animalCardEasyFirst].removeEventListener("click", turnAnimalCard);
+        animalCardEasy[animalCardEasySecond].removeEventListener("click", turnAnimalCard);
+        animalCardEasy[animalCardEasyFirst].classList.add('match');
+        animalCardEasy[animalCardEasySecond].classList.add('match');
     } else {
         stepsCounter();
         setTimeout(changeCardBack, 600); /* Set time how long the two cards would show to users before they turn back unless they are a match */
         function changeCardBack() {
-            animalCardMedium[animalCardMediumFirst].setAttribute('src', './assets/images/question-mark.png');
-            animalCardMedium[animalCardMediumSecond].setAttribute('src', './assets/images/question-mark.png');
-            animalCardMedium[animalCardMediumFirst].setAttribute('alt', 'Card back, select to turn');
-            animalCardMedium[animalCardMediumSecond].setAttribute('alt', 'Card back, select to turn');
+            animalCardEasy[animalCardEasyFirst].setAttribute('src', './assets/images/question-mark.png');
+            animalCardEasy[animalCardEasySecond].setAttribute('src', './assets/images/question-mark.png');
+            animalCardEasy[animalCardEasyFirst].setAttribute('alt', 'Card back, select to turn');
+            animalCardEasy[animalCardEasySecond].setAttribute('alt', 'Card back, select to turn');
         };
     }
-    animalCardMediumSelected = [];
-    animalCardMediumSelectedId = [];
-    scoreCount.textContent = animalCardMediumCorrect.length;
-    if (animalCardMediumCorrect.length === animalCards.length / 2) {
+    animalCardEasySelected = [];
+    animalCardEasySelectedId = [];
+    scoreCount.textContent = animalCardEasyCorrect.length;
+    if (animalCardEasyCorrect.length === animalCards.length / 2) {
         setTimeout(correctMatch, 200);
     }
 }
 
 /* Reloading the game with Reload button or Level seletor */
-document.getElementById('reload').addEventListener('click', resetMediumGame);
-document.getElementById('mediumLevel').addEventListener('click', resetMediumGame);
+document.getElementById('reload').addEventListener('click', resetEasyGame);
+document.getElementById('easylevel').addEventListener('click', resetEasyGame);
 
-function resetMediumGame() {
+function resetEasyGame() {
     animalCardsSelected = [];
     animalCardsSelectedId = [];
     animalCardsCorrect = [];
     cards = document.querySelectorAll('img');
-    animalCardMedium.sort(() => 0.5 - Math.random());
+    animalCardEasy.sort(() => 0.5 - Math.random());
     cards.forEach((c) => {
         c.setAttribute('src', './assets/images/question-mark.png');
         c.addEventListener('click', turnAnimalCard);
